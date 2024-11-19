1ba1cf3d6b8f66cd3d917ec5fd6af16ba1816f55
betich <betich365@gmail.com>
2024-11-19 19:18:35 +0700

    chore: replace search music data with mock data

--- frontend/src/components/search/drawer.tsx
+++ frontend/src/components/search/drawer.tsx
@@ -1,21 +1,46 @@
-import { useMusic } from "@/hooks/useMusic";
 import MusicSearchCard from "./music_search_card.client";
+
+const mockMusic = [
+  {
+    title: "Something",
+    artist: "The Beatles",
+    duration: "3:00",
+    cover: "https://placehold.co/150x150.png",
+  },
+  {
+    title: "Hey Jude",
+    artist: "The Beatles",
+    duration: "4:00",
+    cover: "https://placehold.co/150x150.png",
+  },
+  {
+    title: "APT.",
+    artist: "ROSÉ, Bruno Mars",
+    duration: "2:14",
+    cover: "https://placehold.co/150x150.png",
+  },
+  {
+    title: "APT.",
+    artist: "ROSÉ, Bruno Mars",
+    duration: "2:14",
+    cover: "https://placehold.co/150x150.png",
+  },
+];
 
 export default function SearchDrawer({ search }: { search: string }) {
   console.log("s", search);
-  const { music } = useMusic();
 
   return (
     <div className="w-full bg-white flex flex-col gap-4 absolute top-20 border border-gray-100 shadow-lg p-4 rounded-lg">
       <h1 className="text-xl font-lg">ค้นหาเพลง</h1>
 
       <div className="flex flex-col gap-2">
-        {music.map((m) => (
+        {mockMusic.map((m, i) => (
           <MusicSearchCard
             artist={m.artist}
             cover={m.cover}
             duration={m.duration}
-            key={m.position}
+            key={`${i}-${m.title}`}
             title={m.title}
           />
         ))}
