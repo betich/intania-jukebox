49300f336177aa2ef55cf7b9589a0f700f3af2bf
betich <betich365@gmail.com>
2024-11-24 18:40:06 +0700

    chore: change from deprecated images.domain to images.remotePatterns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["placehold.co", "i.scdn.co"],
+    remotePatterns: [{ hostname: "placehold.co" }, { hostname: "i.scdn.co" }],
   },
 };
 
