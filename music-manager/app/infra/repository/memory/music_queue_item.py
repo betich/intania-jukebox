682784649360a4a87adc3f32d78219c96b32cf7b
betich <betich365@gmail.com>
2024-12-01 01:46:20 +0700

    feat: change singleton implementation to memory object

--- music-manager/app/infra/repository/memory/music_queue_item.py
+++ music-manager/app/infra/repository/memory/music_queue_item.py
@@ -1,28 +1,29 @@
 from app.core.entity.music_queue_item import MusicQueueItem
 from app.core.repository.music_queue_item import MusicQueueItemRepository
 from typing import List
-from app.utils.singleton import Singleton
 
-class MusicQueueItemRepository(Singleton, MusicQueueItemRepository):
+music_queue_item_list: List[MusicQueueItem] = []
+
+class MusicQueueItemRepository(MusicQueueItemRepository):
   def __init__(self):
-    self._music_queue_item_list: List[MusicQueueItem] = []
+    pass
     
   def find_all(self) -> List[MusicQueueItem]:
-    return self._music_queue_item_list
+    return music_queue_item_list
   
   def find_by_id(self, id: str) -> MusicQueueItem | None:
-    for item in self._music_queue_item_list:
+    for item in music_queue_item_list:
       if item.get_song().get_id() == id:
         return item
     return None
   
   def create(self, music: MusicQueueItem) -> MusicQueueItem:
-    self._music_queue_item_list.append(music)
+    music_queue_item_list.append(music)
     return music
   
   def update(self, id: str, new_music: dict) -> MusicQueueItem | None:
     # change the music queue item with the given id by the key-value pairs in new_music
-    for _, item in enumerate(self._music_queue_item_list):
+    for _, item in enumerate(music_queue_item_list):
       if item.get_song().get_id() == id:
         for key, value in new_music.items():
           if hasattr(item, key):
@@ -30,8 +31,8 @@ class MusicQueueItemRepository(Singleton, MusicQueueItemRepository):
         return item
   
   def delete(self, id: str) -> None:
-    for index, item in enumerate(self._music_queue_item_list):
+    for index, item in enumerate(music_queue_item_list):
       if item.get_song().get_id() == id:
-        del self._music_queue_item_list[index]
+        del music_queue_item_list[index]
         return None
     return
--- music-manager/app/infra/repository/memory/song.py
+++ music-manager/app/infra/repository/memory/song.py
@@ -1,35 +1,37 @@
 from app.core.entity.song import Song
 from app.core.repository.song import SongRepository
-from app.utils.singleton import Singleton
+from typing import List
 
-class SongRepository(Singleton, SongRepository):
+song_list: List[Song] = []
+
+class SongRepository(SongRepository):
   def __init__(self):
-    self._song_list = []
+    pass
     
   def find_all(self) -> list[Song]:
-    return self._song_list
+    return song_list
   
   def find_by_id(self, id: str) -> Song | None:
-    for song in self._song_list:
+    for song in song_list:
       if song.id == id:
         return song
     return None
   
   def create(self, song: Song) -> Song:
-    self._song_list.append(song)
+    song_list.append(song)
     return song
   
   def update(self, id: str, song: Song) -> Song | None:
-    for index, song in enumerate(self._song_list):
+    for index, song in enumerate(song_list):
       if song.id == id:
-        self._song_list[index] = song
+        song_list[index] = song
         return song
     return None
   
   def delete(self, id: str) -> None:
-    for index, song in enumerate(self._song_list):
+    for index, song in enumerate(song_list):
       if song.id == id:
-        del self._song_list[index]
+        del song_list[index]
         return None
     return None
  
