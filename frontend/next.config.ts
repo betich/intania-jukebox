cce348486abf2bb6d124113b5006d8838e2a8f5f
betich <betich365@gmail.com>
2024-12-11 02:09:27 +0700

    feat: unoptimize images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [{ hostname: "placehold.co" }, { hostname: "i.scdn.co" }],
+    unoptimized: true,
   },
 };
 
